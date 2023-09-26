e7e8777e30d78e34bd842f870891a463e5290557
Tetra <87898541+Teetras@users.noreply.github.com>
2023-09-26 20:30:49 +0300

    Update Main.java

--- Laba/src/Main.java
+++ Laba/src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
 
-    public void first_commit(){
-        System.out.println("First coomit");
-    }
+    // public void first_commit(){
+    //     System.out.println("First coomit");
+    // }
 
     public static void main(String[] args) {
         Main mai = new Main();
         System.out.println("Hello world!");
-        mai.first_commit();
+        // mai.first_commit();
 
     }
 }
