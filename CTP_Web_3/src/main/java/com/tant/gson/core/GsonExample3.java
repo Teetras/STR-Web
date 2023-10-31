2231dbf1112bac6a3071548656d37a31f53555ac
Teetras <tanya.prokhorenko.2017@gmail.com>
2023-10-31 14:46:46 +0300

    first commit

---
+++ CTP_Web_3/src/main/java/com/tant/gson/core/GsonExample3.java
@@ -0,0 +1,35 @@
+package com.tant.gson.core;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.tant.gson.domain.CustomExclusionStrategy;
+import com.tant.gson.domain.Staff;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class GsonExample3 {
+
+    public static void main(String[] args){
+
+        Gson gson = new GsonBuilder()
+                .setExclusionStrategies(new CustomExclusionStrategy(List.class))
+        //exclude all List fields
+         .create();
+
+        try (Reader reader = new FileReader("D:\\STR-Web\\CTP_Web_3\\src\\main\\resources\\student.json")) {
+
+            Type collectionType = new TypeToken<List<Staff>>(){}.getType();
+            List<Staff>  lcs = (List<Staff>) new Gson().fromJson( reader , collectionType);
+// print staff
+            System.out.println(lcs);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
